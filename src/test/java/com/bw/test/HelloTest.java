837903f1f71f933c633c77574df049b635bf8826
wuzezhou <2665261796@qq.com>
2020-02-26 13:32:52 +0800

    第二次提交

---
+++ src/test/java/com/bw/test/HelloTest.java
@@ -0,0 +1,14 @@
+package com.bw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test//²âÊÔÀà
+	public void test() {
+		System.out.println("µ¥Ôª²âÊÔ");
+	}
+
+}
